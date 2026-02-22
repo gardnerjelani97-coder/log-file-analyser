9e59896496662369bd38b865ba9a3dfca517c64c
gardnerjelani97-coder <gardner.jelani97@gmail.com>
2026-02-22 13:40:18 +0000

    Detect failed login attempts

--- log_analyser.py
+++ log_analyser.py
@@ -1,5 +1,10 @@
-with open("auth.log", "r") as file:
-    logs = file.readlines()
+failed_attempts = []
 
-for line in logs:
-    print(line.strip())
+with open("auth.log", "r") as file:
+    for line in file:
+        if "FAILED_LOGIN" in line:
+            failed_attempts.append(line.strip())
+
+print("Failed Login Attempts:")
+for attempt in failed_attempts:
+    print(attempt)
